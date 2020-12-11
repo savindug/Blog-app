4fe48cb78bbc3c24bdb067dbee5c88b1fdb2bdc2
Savindu Gunawardena <savibashnon11@gmail.com>
2020-12-11 13:43:44 +0530

    Added routes for home, about, feed pages

--- app.py
+++ app.py
@@ -1,10 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
+from feed import Feed
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+Posts = Feed()
+
+
 @app.route('/')
 def index():
-        return render_template('home.html')
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/feed')
+def feed():
+    return render_template('feed.html', posts=Posts)
+
+
+@app.route('/post/<string:id>/')
+def getPost(id):
+    #     for p in Posts:
+    #         if p.id == id:
+    #             print(p)
+    return render_template('post.html', post=id)
+
+
+class RegisterForm (Form):
+    name = StringField('Name', [validators.length(min=1, max=50)])
+    username = StringField('Username', [validators.length(min=4, max=25)])
+    email = StringField('Email', [validators.length(min=1, max=50)])
+    password = PasswordField('Password', [
+        validators.data_required(),
+        validators.EqualTo('confirm', message='Password do not match')
+    ])
+    confirm = PasswordField('Confim Password')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html', form=form)
+    return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ feed.py
@@ -0,0 +1,37 @@
+def Feed():
+    posts = [
+        {
+            'id': 1,
+            'title': 'Post 1',
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+            'author': 'Auothor 1',
+            'create_date': '12-12-2020',
+            'last_modified': '12-12-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Post 2',
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+            'author': 'Auothor 2',
+            'create_date': '11-12-2020',
+            'last_modified': '11-12-2020'
+        },
+        {
+            'id': 3,
+            'title': 'Post 3',
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+            'author': 'Auothor 3',
+            'create_date': '12-11-2020',
+            'last_modified': '12-11-2020'
+        },
+        {
+            'id': 4,
+            'title': 'Post 4',
+            'body': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries',
+            'author': 'Auothor 1',
+            'create_date': '12-12-2020',
+            'last_modified': '12-12-2020'
+        },
+    ]
+
+    return posts
