112212789551ddd7b118f93d1235c84aa2aabf93
Savindu Gunawardena <savibashnon11@gmail.com>
2020-12-15 14:47:36 +0530

    Added register function
    configure virtualenv and connect mysql DB

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
+# Config Mysql
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask_blog'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Initialize MYSQL
+
+mysql = MySQL(app);
+
 Posts = Feed()
 
 
@@ -40,16 +51,38 @@ class RegisterForm (Form):
         validators.data_required(),
         validators.EqualTo('confirm', message='Password do not match')
     ])
-    confirm = PasswordField('Confim Password')
+    confirm = PasswordField('Confirm Password')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html', form=form)
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+
+        # Create cursor
+        cur = mysql.connection.cursor()
+
+        # Execute query
+        cur.execute("INSERT INTO users(name, email, username, password) VALUES(%s, %s, %s, %s)",
+                    (name, email, username, password))
+
+        # Commit to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('You have Successfully Registered to the Flask Blog', 'success')
+
+        redirect(url_for('index'))
+
     return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    app.secret_key = '123'
     app.run(debug=True)
